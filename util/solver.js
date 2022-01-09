a52e5425601f519019534f9953c533f243842ae7
Carlos Henríquez <carlosh@alum.mit.edu>
2022-01-09 05:16:01 -0800

    Add more solver utils

--- util/solver.js
+++ util/solver.js
@@ -1,71 +1,84 @@
 // Wordle Solver
-import { VALID_WORDS, ANSWER_WORDS, NONANSWER_WORDS } from "./wordlist";
-// Wordle hint? Take a word that is in answer words?
-// keep track of solved state to see how to get words?
-// absent, correct, present
+import { ANSWER_WORDS } from "./wordlist";
 
+// Set util for intersections
+function intersection(setA, setB) {
+    let _intersection = new Set()
+    for (let elem of setB) {
+        if (setA.has(elem)) {
+            _intersection.add(elem)
+        }
+    }
+    return _intersection
+}
 
+// Choose a random element from an array. Useful for testing by grabbing a random wordle
+function choice(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
 
 // Return words that contain the present letters
 function getWordsWithPresentLetters(presentLetters = []) {
-    return ANSWER_WORDS.filter(word => {
+    return new Set(ANSWER_WORDS.filter(word => {
         for (const letter of presentLetters) {
             if (!(word.includes(letter))) {
                 return false;
             }
         }
         return true;
-    });
+    }));
 }
 
 // Return words that contain the correct letters in the right places
-function getWordsWithCorrectLetters(guesses = [{letter, position}]= [{}]) {
-    return ANSWER_WORDS.filter(word => {
-        for (const {letter, position} of guesses) {
-            if (!(word[position] === letter)) {
+function getWordsWithCorrectLetters(guesses = []) {
+    return new Set(ANSWER_WORDS.filter(word => {
+        for (const [index, letter] of guesses.entries()) {
+            if (letter !== ' ' && !(word[index] === letter)) {
                 return false;
             }
-            return true;
         }
-    });
+        return true;
+    }));
 }
 
 // Return words that don't contain incorrectly guessed letters
 function getWordsWithoutAbsentLetters(absentLetters = []) {
-    return ANSWER_WORDS.filter(word => {
+    return new Set(ANSWER_WORDS.filter(word => {
         for (const absent of absentLetters) {
             if (word.includes(absent)) {
                 return false;
             }
         }
         return true;
-    });
+    }));
 }
 
-function choice(arr) {
-    const index = Math.floor(Math.random() * arr.length);
-    return arr[index];
-}
-
-word = choice(ANSWER_WORDS);
 function gameResult(answer, guess, gameState = {
-    absentLetters = [],
-    presentLetters = [],
-    correctLetters = [{letter, position} = {}]
+    absentLetters: new Set(),
+    presentLetters: new Set(),
+    correctLetters: [' ', ' ', ' ', ' ', ' '] // default to empty
 }) {
     for (let [index, letter] of guess.split('').entries()) {
         if (!(answer.includes(letter))){
-            gameState.absentLetters.push(letter);
+            gameState.absentLetters.add(letter);
+        } else if (answer[index] === letter) {
+            gameState.correctLetters[index] = letter;
         } else {
-            if (answer[index] === letter) {
-                gameState.correctLetters
-            }
+            gameState.presentLetters.add(letter);
         }
     }
     return gameState;
 }
 
-// wordle plus plus
-// Play future/past wordles? just iterate through the array
-// See possible word bank?
-// Get test word?
+// Get possible guess words given the current state
+function getPossibleWords({
+    absentLetters = new Set(),
+    presentLetters = new Set(),
+    correctLetters = [' ', ' ', ' ', ' ', ' '] // default to empty
+} = {}) {
+    absent = getWordsWithoutAbsentLetters(absentLetters);
+    present = getWordsWithPresentLetters(presentLetters);
+    correct = getWordsWithCorrectLetters(correctLetters);
+    return intersection(present, intersection(correct, absent));
+}
